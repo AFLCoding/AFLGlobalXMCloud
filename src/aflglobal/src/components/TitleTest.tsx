08f1693ef12a1388ed6c311821332120bd8ece2a
Tushar <tushar.singh@altudo.co>
2024-03-21 18:12:52 +0530

    removed unwanted file

--- src/aflglobal/src/components/TitleTest.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CareersBanner from './AFL Career Banner/AFL Career Banner';
-
-const TitleTest = () => {
-  return <CareersBanner />;
-};
-
-export default TitleTest;
